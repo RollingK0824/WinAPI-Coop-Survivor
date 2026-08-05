717431a0a8630547d6b70799b083542cd4755329
RollingK0824 <garamei72@gmail.com>
2026-08-05 15:27:12 +0900

    feat(game): TitleSceneController 상세 구현 및 링커 에러(LNK2001) 수정
    - TitleSceneController.h에 선언된 가상 함수(Awake, Serialize, Deserialize)의 구현체를 TitleSceneController.cpp에 추가하여 LNK2001 링커 에러 해소
    - ExposeComponent를 이용한 버튼 Drag & Drop 포인터 인스펙터 노출 설정
    - Host, Join, Exit 버튼 클릭 이벤트 핸들러 및 네트워크 접속 / 씬 전환 로직 추가

--- WinAPI-Coop-Survivor/Source/Game/Scripts/UI/TitleSceneController.cpp
+++ WinAPI-Coop-Survivor/Source/Game/Scripts/UI/TitleSceneController.cpp
@@ -2,14 +2,76 @@
 #include "TitleSceneController.h"
 #include "Engine/Core/ComponentRegister.h"
 #include "Engine/Framework/Components/UI/UIButtonComponent.h"
+#include "Engine/Network/NetworkManager.h"
+#include "Engine/Manager/SceneManager.h"
+#include "Engine/Framework/GameObject.h"
+#include "Engine/Framework/Scene.h"
 
 static ComponentRegistrar<TitleSceneController> registrar(EngineKey::CustomComponent::TitleSceneController.data());
 
-TitleSceneController::TitleSceneController(GameObject* owner, TransformComponent* transform) : ScriptComponent(owner, transform)
+TitleSceneController::TitleSceneController(GameObject* owner, TransformComponent* transform)
+	: ScriptComponent(owner, transform)
 {
+	ExposeComponent("Host Button", &host_Btn);
+	ExposeComponent("Join Button", &join_Btn);
+	ExposeComponent("Exit Button", &exit_Btn);
+}
+
+void TitleSceneController::Awake()
+{
+	ScriptComponent::Awake();
 }
 
 void TitleSceneController::Start()
 {
-	
+	ScriptComponent::Start();
+
+	if (host_Btn != nullptr)
+	{
+		host_Btn->SetOnClick([this]() { OnClickHostBtn(); });
+	}
+
+	if (join_Btn != nullptr)
+	{
+		join_Btn->SetOnClick([this]() { OnClickJoinBtn(); });
+	}
+
+	if (exit_Btn != nullptr)
+	{
+		exit_Btn->SetOnClick([this]() { OnClickExitBtn(); });
+	}
+}
+
+void TitleSceneController::OnClickHostBtn()
+{
+	std::cout << "[TitleSceneController] HOST 버튼 클릭됨!" << std::endl;
+	if (NetworkManager::GetInstance()->StartHost(9000))
+	{
+		SceneManager::GetInstance()->LoadSceneFromFile("Resources/Scenes/InGameScene.scene");
+	}
+}
+
+void TitleSceneController::OnClickJoinBtn()
+{
+	std::cout << "[TitleSceneController] JOIN 버튼 클릭됨!" << std::endl;
+	if (NetworkManager::GetInstance()->ConnectToHost("127.0.0.1", 9000))
+	{
+		SceneManager::GetInstance()->LoadSceneFromFile("Resources/Scenes/InGameScene.scene");
+	}
+}
+
+void TitleSceneController::OnClickExitBtn()
+{
+	std::cout << "[TitleSceneController] EXIT 버튼 클릭됨!" << std::endl;
+	PostQuitMessage(0);
+}
+
+void TitleSceneController::Serialize(json& outJson) const
+{
+	ScriptComponent::Serialize(outJson);
+}
+
+void TitleSceneController::Deserialize(const json& inJson)
+{
+	ScriptComponent::Deserialize(inJson);
 }
